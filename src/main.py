b36eb6b20968bfc99106b76fc1bbf4460bbee303
GuilhermeSvl <guilherme.henriquesvl@gmail.com>
2024-03-23 16:44:39 -0300

    melhorando interface de classe

--- src/main.py
+++ src/main.py
@@ -1,15 +1,8 @@
-#**Descrição do Problema:**
+from leituraArquivos import LeitorArquivo
 
-#Um Autômato Finito Determinístico (AFD) é um modelo matemático abstrato de um sistema computacional que reconhece cadeias de símbolos de uma linguagem regular. Ele é composto por cinco elementos:
+nomeArquivo = input("Digite o nome do arquivo para teste: /'entrada.txt' ")
+diretorio = f'testes/{nomeArquivo}'
 
-#1. **Estados:** Um conjunto finito de estados.
+iniciar = LeitorArquivo(diretorio)
 
-#2. **Alfabeto:** Um conjunto finito de símbolos chamado de alfabeto.
-
-#3. **Transições:** Uma função de transição que mapeia cada par (estado, símbolo) para outro estado.
-
-#4. **Estado Inicial:** Um estado inicial, que é o estado em que o AFD começa a processar a entrada.
-
-#5. **Estados de Aceitação:** Um conjunto de estados de aceitação, que indica os estados nos quais o AFD deve estar para aceitar uma cadeia.
-
-
+iniciar.lerArquivo()
